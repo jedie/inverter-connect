723ea1f825c9ca89bbdf8ad6290e0c5360b0b168
JensDiemer <git@jensdiemer.de>
2023-05-19 21:20:31 +0200

    Enhance logging

--- inverter/tests/__init__.py
+++ inverter/tests/__init__.py
@@ -1,7 +1,7 @@
 import os
 import unittest.util
 
-from inverter.verbosity import setup_logging
+from inverter.verbosity import MAX_LOG_LEVEL, setup_logging
 
 
 # Hacky way to expand the failed test output:
@@ -9,4 +9,4 @@ unittest.util._MAX_LENGTH = os.environ.get('UNITTEST_MAX_LENGTH', 300)
 
 
 # Display DEBUG logs in tests:
-setup_logging(verbosity=3)
+setup_logging(verbosity=MAX_LOG_LEVEL)
--- inverter/verbosity.py
+++ inverter/verbosity.py
@@ -1,13 +1,16 @@
 import logging
 
 import click
+from rich import get_console
 from rich.logging import RichHandler
 
 
+MAX_LOG_LEVEL = 3
+
 OPTION_KWARGS_VERBOSE = dict(
     count=True,
-    type=click.IntRange(0, 3),
-    default=1,
+    type=click.IntRange(0, MAX_LOG_LEVEL),
+    default=0,
     help='Verbosity level; Accepts integer value e.g.: "--verbose 2" or can be count e.g.: "-vv" ',
     show_default=True,
 )
@@ -21,11 +24,16 @@ def setup_logging(*, verbosity: int):
         level = logging.WARNING
     elif verbosity == 2:
         level = logging.INFO
-        log_format = '%(asctime)s %(message)s'
     else:
         level = logging.DEBUG
-        log_format = '%(asctime)s (%(name)s) %(message)s'
+        log_format = '(%(name)s) %(message)s'
 
-    if verbosity:
-        print(f'Set log level to: {logging.getLevelName(level)}')
-    logging.basicConfig(level=level, format=log_format, datefmt='[%X]', handlers=[RichHandler()], force=True)
+    console = get_console()
+    console.print(f'(Set log level {verbosity}: {logging.getLevelName(level)})', justify='right')
+    logging.basicConfig(
+        level=level,
+        format=log_format,
+        datefmt='[%x %X.%f]',
+        handlers=[RichHandler(console=console, omit_repeated_times=False)],
+        force=True,
+    )
