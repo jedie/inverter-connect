1857e55327b000d3a908a2097ffcc4241d5a22b4
JensDiemer <git@jensdiemer.de>
2023-05-26 21:45:00 +0200

    Bugfix TypeError: make_config() got an unexpected keyword argument 'config_path'

--- inverter/user_settings.py
+++ inverter/user_settings.py
@@ -110,7 +110,7 @@ def migrate_old_settings(toml_settings: TomlSettings):  # TODO: Remove in the Fu
             logging.debug('No old settings file found here: %s', v2_settings_path)
 
 
-def make_config(*, user_settings: UserSettings, ip, port, verbosity, inverter=None) -> Config:
+def make_config(*, user_settings: UserSettings, ip, port, verbosity, config_path=None, inverter=None) -> Config:
     # "Validate" ip address:
     try:
         result = socket.gethostbyname(ip)
@@ -125,4 +125,5 @@ def make_config(*, user_settings: UserSettings, ip, port, verbosity, inverter=No
         port=port,
         mqtt_settings=user_settings.mqtt,
         inverter_name=inverter,
+        config_path=config_path,
     )
